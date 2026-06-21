b443891479c24f5aaf45278d13f794bc9ea06d89
sery <sery@sappho.company>
2026-06-22 00:40:08 +0200

    fix flags

---
+++ src/assets/images/flags/flags.ts
@@ -0,0 +1,11 @@
+import _demi from './demi.png'
+import _nonbinary from './non-binary.png'
+import _polyamorous from './polyamogus.png'
+import _demiro from './Demiromantic.png'
+import _dac from './arf.png'
+
+export const demisexual = _demi
+export const nonbinary = _nonbinary
+export const polyamorous = _polyamorous
+export const demiromantic = _demiro
+export const demiaceflux = _dac
