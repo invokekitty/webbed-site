e30c308eff0166619f48f15c96469e1f511b5776
sery <sery@sappho.company>
2026-06-28 23:49:14 +0200

    mraw

--- src/pages/api/meow.ts
+++ src/pages/api/meow.ts
@@ -4,11 +4,11 @@ import { env } from "cloudflare:workers";
 export const prerender = false;
 
 export async function GET({ url }: APIContext): Promise<Response> {
-    const route = url.searchParams.get("route") ?? "/";
-    const current = await env.PERSISTENT_DATA.get(`visits-${route}`).then(s => Number.parseInt(s ?? "0"));
+    const route = url.searchParams.get("route")?.replaceAll('/', '_') ?? "/";
+    const current = await env.PERSISTENT_DATA.get(`visits_${route}`).then(s => Number.parseInt(s ?? "0"));
     const n = current + 1;
     const message = `.visit-counter::after {content: "${n}" !important;}}`;
-    env.PERSISTENT_DATA.put(`visits-${route}`, n.toString()).catch(console.error);
+    env.PERSISTENT_DATA.put(`visits_${route}`, n.toString()).catch(console.error);
 
     return new Response(message, {
         headers: {
